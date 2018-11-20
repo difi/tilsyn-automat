aefc8270f52f22a480eb4509d1f7a9436771d62d
Henrik Juhlin <henrik.juhlin@funka.com>
2018-11-20 09:08:20 +0100

    more work with follow up questions

--- wwwroot/wwwroot/js/declarationForm.js
+++ wwwroot/wwwroot/js/declarationForm.js
@@ -1,23 +1,32 @@
 ﻿$(function () {
-    console.log($('[data-ViewIfParentFailed="True"]').length);
+    console.log($('[data-viewifotherfailed="True"]').length);
 
-    $('[data-ViewIfParentFailed="True"]').hide();
+    $('[data-viewifotherfailed="True"]').hide();
 
     $(".jsAnswerItem").change(function () {
         var answerItem = $(this);
 
         var type = $(this).data("type");
         var id = $(this).data("id");
-        var bool = $(this).data("bool");
+        var bool = $(this).data("bool").toLowerCase();
         var min = $(this).data("min");
         var max = $(this).data("max");
-
+        var viewifotherfailed = $(this).data("data-viewifotherfailed");
+        
         if (type === "bool") {
+
+            console.log("Hmmm1");
+
             $(this).find("input:checked").each(function () {
+
+                console.log("Hmmm2");
+
                 if ($(this).val() === bool) {
+                    //answerItem.removeClass("errorOnThis");
                     answerItem.data("correct", true);
                     answerItem.next().next().hide();
                 } else {
+                    //answerItem.addClass("errorOnThis");
                     answerItem.data("correct", false);
                     answerItem.next().next().show();
                 }
@@ -26,5 +35,6 @@
 
         console.log(id);
         console.log(type);
+        console.log(bool);
     });
 });
