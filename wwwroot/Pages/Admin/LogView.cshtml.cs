f6fa4e7aab86b4ea0dd421090623a72667aaa4e1
Henrik Juhlin <henrik.juhlin@funka.com>
2018-12-09 11:03:21 +0100

    Added logview for single item

---
+++ wwwroot/Pages/Admin/LogView.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Difi.Sjalvdeklaration.Shared.Classes;
+using Difi.Sjalvdeklaration.Shared.Classes.User;
+using Difi.Sjalvdeklaration.wwwroot.Business.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Difi.Sjalvdeklaration.wwwroot.Pages.Admin
+{
+    public class LogViewModel : PageModel
+    {
+        private readonly IApiHttpClient apiHttpClient;
+
+        public LogItem LocalizationItem { get; set; }
+
+        public LogViewModel(IApiHttpClient apiHttpClient)
+        {
+            this.apiHttpClient = apiHttpClient;
+        }
+
+        public LogItem LogItem { get; private set; }
+
+        public UserItem UserItem { get; private set; }
+
+        [HttpGet]
+        public async Task OnGetAsync(Guid id)
+        {
+            LogItem = (await apiHttpClient.Get<LogItem>("/api/Log/Get/"+id)).Data;
+
+            if (LogItem.UserId != Guid.Empty)
+            {
+                UserItem = (await apiHttpClient.Get<UserItem>("/api/User/Get/" + LogItem.Id)).Data;
+            }
+
+
+        }
+    }
+}
