55342b4d2f361e676ab618161b03dc1cf1311596
Henrik Juhlin <henrik.juhlin@funka.com>
2018-12-13 16:37:44 +0100

    Added excel export to form for single items

--- wwwroot/Pages/Admin/DeclarationList.cshtml.cs
+++ wwwroot/Pages/Admin/DeclarationList.cshtml.cs
@@ -1,27 +1,17 @@
-﻿using Difi.Sjalvdeklaration.Shared.Attributes;
-using Difi.Sjalvdeklaration.Shared.Classes.Company;
-using Difi.Sjalvdeklaration.Shared.Classes.Declaration;
-using Difi.Sjalvdeklaration.Shared.Classes.User;
+﻿using Difi.Sjalvdeklaration.Shared.Classes.Declaration;
 using Difi.Sjalvdeklaration.Shared.Classes.ValueList;
 using Difi.Sjalvdeklaration.Shared.Extensions;
+using Difi.Sjalvdeklaration.wwwroot.Business;
 using Difi.Sjalvdeklaration.wwwroot.Business.Interface;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Localization;
-using OfficeOpenXml;
-using OfficeOpenXml.Style;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Data;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
-using Difi.Sjalvdeklaration.Shared.Classes;
-using Difi.Sjalvdeklaration.wwwroot.Business;
 
 namespace Difi.Sjalvdeklaration.wwwroot.Pages.Admin
 {
@@ -122,28 +112,6 @@ namespace Difi.Sjalvdeklaration.wwwroot.Pages.Admin
         public async Task<IActionResult> OnPostViewAllAsync()
         {
             return await errorHandler.View(this, OnGetAsync());
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> OnPostExportDeclarationAsync(string id)
-        {
-            try
-            {
-                var result = await apiHttpClient.Get<DeclarationItem>("/api/Declaration/Get/" + Guid.Parse(id));
-
-                if (result.Succeeded)
-                {
-                    var data = excelGenerator.GenerateExcel(new List<DeclarationItem> { result.Data });
-
-                    return File(data, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", $"{result.Data.Name} ({DateTime.Now.GetAsFileName()}).xlsx");
-                }
-
-                return await errorHandler.View(this, OnGetAsync(), result.Exception);
-            }
-            catch (Exception exception)
-            {
-                return await errorHandler.Log(this, OnGetAsync(), exception, id);
-            }
         }
 
         [HttpPost]
