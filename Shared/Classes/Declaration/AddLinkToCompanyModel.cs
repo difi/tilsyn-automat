54bbbae6d28deb318ec328a170b4672d943bb24b
Henrik Juhlin <henrik.juhlin@funka.com>
2018-12-18 23:10:43 +0100

    changed fields to numbers

--- Shared/Classes/Declaration/AddLinkToCompanyModel.cs
+++ Shared/Classes/Declaration/AddLinkToCompanyModel.cs
@@ -6,9 +6,11 @@ namespace Difi.Sjalvdeklaration.Shared.Classes.Declaration
     {
         [Required(ErrorMessage = "The organization number of your business is required")]
         [Display(Name = "Organization number of your business")]
+        [RegularExpression("\\d{9}", ErrorMessage = "Organization number must have nine numbers")]
         public string CorporateIdentityNumber { get; set; }
 
         [Required(ErrorMessage = "Four-digit code is required")]
+        [RegularExpression("\\d{4}", ErrorMessage = "Four-digit code must have four numbers")]
         [Display(Name = "Four-digit code")]
         public string Code { get; set; }
     }
